2e5801c9ff9ee593e1cb01c24e9ca0a1cd2a030a
Andy Muir <muirandy@yahoo.co.uk>
2019-11-04 09:30:51 +0000

    Ensure Schema Registry is working before starting tests

--- src/test/java/com/aimyourtechnology/kafka/oracle/service/ServiceTest.java
+++ src/test/java/com/aimyourtechnology/kafka/oracle/service/ServiceTest.java
@@ -10,11 +10,13 @@ import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.ByteBuffer;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,6 +24,59 @@ public class ServiceTest extends ServiceTestEnvironmentSetup {
     private static final String INPUT_TOPIC = "input";
     private static final String OUTPUT_TOPIC = "output";
     private static final int SUBJECT_VERSION_1 = 1;
+    private static final boolean SUBJECTS_RETRIEVED_SUCCESSFULLY = true;
+    private static final boolean SUBJECTS_NOT_RETRIEVED = false;
+    private static final int SECONDS_TO_WAIT_FOR_SCHEMA_REGISTRY_TO_START = 60;
+    private int schemaRegistryRetryCount = 0;
+
+    @BeforeEach
+    void setUp() {
+        ensureSchemaRegistryIsReady();
+    }
+
+    private void ensureSchemaRegistryIsReady() {
+        SchemaRegistryClient schemaRegistryClient = new CachedSchemaRegistryClient(getSchemaRegistryUrl(), 20);
+        boolean schemaRegistryIsReady = false;
+        initialiseRetryCount();
+        do {
+            giveSchemaRegistryAnOpportunityToStart();
+            schemaRegistryIsReady = tryToRetrieveSubjects(schemaRegistryClient);
+            incrementRetryCount();
+        } while (!schemaRegistryIsReady && !schemRegistryRetryCountExceeded());
+    }
+
+    private String getSchemaRegistryUrl() {
+        return "http://localhost:8081";
+    }
+
+    private void initialiseRetryCount() {
+        schemaRegistryRetryCount = 0;
+    }
+
+    private void giveSchemaRegistryAnOpportunityToStart() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean tryToRetrieveSubjects(SchemaRegistryClient schemaRegistryClient) {
+        try {
+            schemaRegistryClient.getAllSubjects();
+            return SUBJECTS_RETRIEVED_SUCCESSFULLY;
+        } catch (IOException | RestClientException e) {
+            return SUBJECTS_NOT_RETRIEVED;
+        }
+    }
+
+    private void incrementRetryCount() {
+        schemaRegistryRetryCount++;
+    }
+
+    private boolean schemRegistryRetryCountExceeded() {
+        return schemaRegistryRetryCount >= SECONDS_TO_WAIT_FOR_SCHEMA_REGISTRY_TO_START;
+    }
 
     @Test
     void bytesAreCastToInteger() {
@@ -33,10 +88,6 @@ public class ServiceTest extends ServiceTestEnvironmentSetup {
 
     private void createSchemaForInputTopic() {
         createSchema(INPUT_TOPIC + "-value", createInputByteSchema());
-    }
-
-    private void createSchemaForOutputTopic() {
-        createSchema(OUTPUT_TOPIC + "-value", createOutputIntegerSchema());
     }
 
     private void createSchema(String subject, Schema schema) {
@@ -52,37 +103,30 @@ public class ServiceTest extends ServiceTestEnvironmentSetup {
 
     private Schema createInputByteSchema() {
         return SchemaBuilder.record("root")
-                            .fields()
-                            .name("id")
-                            .type(Schema.create(Schema.Type.STRING))
-                            .noDefault()
-                            .name("amount")
-                            .type(Schema.create(Schema.Type.BYTES))
-                            .noDefault()
-                            .endRecord();
+                .fields()
+                .name("id")
+                .type(Schema.create(Schema.Type.STRING))
+                .noDefault()
+                .name("amount")
+                .type(Schema.create(Schema.Type.BYTES))
+                .noDefault()
+                .endRecord();
+    }
+
+    private void createSchemaForOutputTopic() {
+        createSchema(OUTPUT_TOPIC + "-value", createOutputIntegerSchema());
     }
 
     private Schema createOutputIntegerSchema() {
         return SchemaBuilder.record("root")
-                            .fields()
-                            .name("id")
-                            .type(Schema.create(Schema.Type.STRING))
-                            .noDefault()
-                            .name("amount")
-                            .type(Schema.create(Schema.Type.INT))
-                            .noDefault()
-                            .endRecord();
-    }
-
-    private GenericRecord createInputAvroMessage() {
-        Schema inputSchema = obtainSchema(INPUT_TOPIC + "-value");
-        GenericRecord message = new GenericData.Record(inputSchema);
-        byte[] bytes = new BigInteger("1000").toByteArray();
-        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
-        //        message.put("root", );
-        message.put("id", orderId);
-        message.put("amount", byteBuffer);
-        return message;
+                .fields()
+                .name("id")
+                .type(Schema.create(Schema.Type.STRING))
+                .noDefault()
+                .name("amount")
+                .type(Schema.create(Schema.Type.INT))
+                .noDefault()
+                .endRecord();
     }
 
     private void assertAvroMessageHasAppliedCast() {
@@ -96,17 +140,14 @@ public class ServiceTest extends ServiceTestEnvironmentSetup {
         //        records.forEach(topicConsumer);
     }
 
-    private void assertRecordValueAvro(ConsumerRecord<String, GenericRecord> consumerRecord) {
-        GenericRecord value = consumerRecord.value();
-        GenericRecord expectedValue = createExpectedAvroMessage();
-        assertEquals(expectedValue, value);
-    }
-
-    private GenericRecord createExpectedAvroMessage() {
-        Schema schema = obtainSchema(OUTPUT_TOPIC + "-value");
-        GenericRecord message = new GenericData.Record(schema);
+    private GenericRecord createInputAvroMessage() {
+        Schema inputSchema = obtainSchema(INPUT_TOPIC + "-value");
+        GenericRecord message = new GenericData.Record(inputSchema);
+        byte[] bytes = new BigInteger("1000").toByteArray();
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+        //        message.put("root", );
         message.put("id", orderId);
-        message.put("amount", 1000);
+        message.put("amount", byteBuffer);
         return message;
     }
 
@@ -128,8 +169,18 @@ public class ServiceTest extends ServiceTestEnvironmentSetup {
         return schemaRegistryClient.getById(schemaMetadata.getId());
     }
 
-    private String getSchemaRegistryUrl() {
-        return "http://localhost:8081";
+    private void assertRecordValueAvro(ConsumerRecord<String, GenericRecord> consumerRecord) {
+        GenericRecord value = consumerRecord.value();
+        GenericRecord expectedValue = createExpectedAvroMessage();
+        assertEquals(expectedValue, value);
+    }
+
+    private GenericRecord createExpectedAvroMessage() {
+        Schema schema = obtainSchema(OUTPUT_TOPIC + "-value");
+        GenericRecord message = new GenericData.Record(schema);
+        message.put("id", orderId);
+        message.put("amount", 1000);
+        return message;
     }
 
     @Override
